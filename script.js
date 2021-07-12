39259cdf6dfd2f622a8207b8c354be8ac26fa9ee
jose <delossantosramosjose@gmail.com>
2021-07-12 17:47:30 +0200

    added input to find emojis

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let EMOJILIST
+
 window.onload = function () {
     showEmojis()
+    document.getElementById("search").addEventListener("keyup", find)
 }
 
 function showEmojis() {
@@ -8,6 +11,7 @@ function showEmojis() {
     }).then(res => res.json())
         .catch(error => console.error('Error:', error))
         .then(res => {
+            EMOJILIST = res
             let emojis = ""
             for (let emoji in res) {
                 emojis += `
@@ -18,4 +22,24 @@ function showEmojis() {
                 `}
             document.getElementById("emojis").innerHTML = emojis
         })
+}
+
+function find(event) {
+    let emojis = ""
+
+    for (let emoji in EMOJILIST) {
+        let ExpReg = new RegExp("" + event.target.value + "", "g")
+
+        if (emoji.match(ExpReg))
+            emojis += `
+        <div class="emoji">
+            <img src="${EMOJILIST[emoji]}" alt="${emoji}" loading="lazy">
+            <p>:${emoji}:</p>
+        </div>
+        `}
+
+    if (emojis == "")
+        emojis = "no matches"
+
+    document.getElementById("emojis").innerHTML = emojis
 }
